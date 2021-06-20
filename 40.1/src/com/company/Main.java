b8dbede0cdb9cd9a204cbf98d445b949b0a85d42
Nurdin954 <84643087+Nurdin954@users.noreply.github.com>
2021-06-20 18:13:50 +0600

    Add files via upload

---
+++ 40.1/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+
+        HashSet<String> set = new HashSet<String>();
+        set.add("Ravi");
+        set.add("Vitaliy");
+        set.add("Ravi");
+        set.add("Emir");
+        //Traversing elements
+        Iterator<String> itr = set.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+        System.out.println("*--------------------------------------*");
+
+        Set<String> linkedHashSet = new LinkedHashSet<>();
+
+        linkedHashSet.add("Бета");
+        linkedHashSet.add("Aльфa");
+        linkedHashSet.add("Гaммa");
+        linkedHashSet.add("Гaммa");
+        linkedHashSet.add("Эпсилон");
+        linkedHashSet.add("Oмeгa");
+
+        System.out.println(linkedHashSet);
+    }
+}
+
